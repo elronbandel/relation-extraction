e68672685becf6f92a9cb9b8bc888928a497e03a
elron <elronbandel@gmail.com>
2020-01-16 20:01:06 +0200

    inital

---
+++ dataset.py
@@ -0,0 +1,37 @@
+from torchtext.datasets import SequenceTaggingDataset
+from torchtext.data import Field, BucketIterator
+from torchtext import data, datasets, vocab
+
+
+class RexDataset():
+    def __init__(self, path='', glove_name='6B', glove_dim=300):
+        fields = [
+            ('text', Field(include_lengths=True, sequential=True)),
+            ('cand1', Field( dtype=int)),
+            ('cand2', Field(dtype=int)),
+            ('label', Field(is_target=True))
+        ]
+        self.train_set, self.dev_set = SequenceTaggingDataset.splits(path=path, train='train.tsv', validation='dev.tsv',fields = fields)
+        self.fields = dict(fields)
+        self.fields['text'].build_vocab(self.train_set, self.dev_set, vectors=vocab.GloVe(name=glove_name, dim=glove_dim))
+        self.fields['cand1'].build_vocab(self.train_set)
+        self.fields['cand2'].build_vocab(self.train_set)
+        self.fields['label'].build_vocab(self.train_set)
+
+
+    def train(self, batch_size, device):
+        return BucketIterator(self.train_set, batch_size=batch_size, device=device)
+
+    def dev(self, batch_size, device):
+        return BucketIterator(self.dev_set, batch_size=batch_size, device=device)
+
+
+
+
+
+if __name__ == "__main__":
+    ds = RexDataset(path='dataset')
+    batch = next(iter(ds.train(2, 'cuda:0')))
+    ((sent, len), cand1, cand2), target = batch
+    print(sent)
+
---
+++ preparing_data/generating_data.py
@@ -0,0 +1,132 @@
+import spacy
+import difflib
+from collections import defaultdict
+
+# loading the corpus itself:
+def load_corpus(path):
+    sentences = dict()
+    with open(path) as corpus:
+        for line in corpus:
+            tokens = line.split()
+            sentences[int(tokens[0][4:])] = ' '.join(tokens[1:])
+    return sentences
+
+#loadig the annotation of the corpus
+def load_annotations(path):
+    annots = defaultdict(list)
+    with open(path) as data:
+        for line in data:
+            tokens = line.split('\t')
+            annots[int(tokens[0][4:])].append(tokens[1:])
+    return annots
+
+def ent(sentences, nlp):
+    vocab = set()
+    for sentence in sentences:
+        for token in nlp(sentence):
+            vocab.add(token.text)
+    return vocab
+
+def corpus_entetities(corpus, nlp):
+    ents = set()
+    for id, sent in corpus.items():
+        for ent in nlp(sent).ents:
+            ents.add(ent.text)
+    return ents
+
+def anotations_entitites(anotations):
+    ents = set()
+    for id, anots in anotations.items():
+        for anot in anots:
+            ents.add(anot[0]), ents.add(anot[2])
+    return ents
+
+relsyms = {'Work_For':'WRK', 'Live_In':'LIV'}
+
+def normalize_entitiy(raw_ent, sent, nlp):
+    ents = set(map(str, nlp(sent).ents))
+    if raw_ent in ents:
+        return raw_ent
+    for ent in ents:
+        ent = str(ent)
+        if ent in raw_ent or raw_ent in ent:
+            return ent
+    return raw_ent
+
+
+
+def process_one_anotation(anot, nlp):
+    arg1, rel, arg2, sent = anot
+    sent = sent[1:-1].replace("-LRB-", "(").replace("-RRB-", ")")
+    arg1 = normalize_entitiy(arg1, sent, nlp)
+    arg2 = normalize_entitiy(arg2, sent, nlp)
+    rel = relsyms[rel]
+    return (arg1, rel, arg2, sent)
+
+
+def process_annotations(annotations, nlp):
+    res = defaultdict(list)
+    for id, anots in annotations.items():
+        for anot in anots:
+            if anot[1] in relsyms:
+                res[id].append(process_one_anotation(anot, nlp))
+    return res
+
+def extract_sentence(sentence, annotations, nlp):
+    processed = nlp(sentence)
+    tokenized = [token.text for token in processed]
+
+from itertools import combinations
+
+def annotation_record(sentence, candidate1,  candidate2, label):
+    return "\n".join([token if i !=0 else '\t'.join([token, str(candidate1), str(candidate2), label]) for i, token in enumerate(sentence)])
+
+def generate_data(corpus, annots, nlp):
+    data = []
+    for id, sent in corpus.items():
+        text, ents = [], []
+        for i, token in enumerate(nlp(sent)):
+            text.append(token.text)
+            if not token.ent_type_ == '':
+                ents.append((token.text, i))
+        for (arg1, i1), (arg2, i2) in combinations(ents, 2):
+            for annot in annots[id]:
+                if arg1 == annot[0] and arg2 == annot[2]:
+                    rel = f'{annot[1]}->'
+                elif arg2 == annot[0] and arg1 == annot[2]:
+                    rel = f'{annot[1]}<-'
+                else:
+                    rel = 'NON'
+                data.append(annotation_record(text, i1, i2, rel))
+    return data
+
+def make_tsv(section, nlp):
+    corpus = load_corpus(f'data/Corpus.{section}.txt')
+    annotations = load_annotations(f'data/{section}.annotations')
+    processed = process_annotations(annotations, nlp)
+    data = generate_data(corpus, processed, nlp)
+    open(f'{section.lower()}.tsv', 'w+').write("\n\n".join(data))
+
+
+def check_intersection(corpus, annots):
+    corp_ents = corpus_entetities(corpus, nlp)
+    anot_ents = anotations_entitites(annots)
+    print(corp_ents)
+    print(anot_ents)
+    inter = anot_ents.intersection(corp_ents)
+    print(len(inter) / len(anot_ents))
+    failed = anot_ents.difference(corp_ents)
+    print(sorted(list(failed)))
+    print(len(failed))
+
+
+
+
+if __name__ == "__main__":
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe(nlp.create_pipe('merge_entities'))
+    make_tsv('TRAIN', nlp)
+    make_tsv('DEV', nlp)
+
+
+
