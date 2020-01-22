69459892afd1c78f7c8fe7a542e7a49d5556296d
judaj <58119953+judaj@users.noreply.github.com>
2020-01-22 16:25:12 -0600

    Add files via upload

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,15 +1,15 @@
-from datatools.load import read_csv_to_dicts, data_label_split
+from datatools.load import read_csv_to_dicts, data_annot_split
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.preprocessing import LabelEncoder
 
 class DataSet:
     def __init__(self, dir):
         self.dir = dir
-        self.train_data, self.train_labels = self.load('train.csv')
-        self.dev_data, self.dev_labels = self.load('dev.csv')
+        self.train_data, self.train_labels, self.train_annot_data = self.load('train.csv')
+        self.dev_data, self.dev_labels, self.dev_annot_data = self.load('dev.csv')
         self.data_vec, self.labels_vec = DictVectorizer(), LabelEncoder()
         self.train = self.data_vec.fit_transform(self.train_data), self.labels_vec.fit_transform(self.train_labels)
         self.dev = self.data_vec.transform(self.dev_data), self.labels_vec.transform(self.dev_labels)
 
     def load(self, file):
-        return data_label_split(read_csv_to_dicts(self.dir + '/' + file))
+        return data_annot_split(read_csv_to_dicts(self.dir + '/' + file))
