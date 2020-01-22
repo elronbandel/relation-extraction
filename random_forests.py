dc2045d6a47329f4f57e83f9e5dd9d9befbe4652
judaj <58119953+judaj@users.noreply.github.com>
2020-01-22 16:27:04 -0600

    Add files via upload

--- random_forests.py
+++ random_forests.py
@@ -1,5 +1,6 @@
 from dataset.dataset import DataSet
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 # Load numpy
@@ -7,6 +8,14 @@ import numpy as np
 
 # Set random seed
 #np.random.seed(3)
+good_annot = ["Live_In", "NONE", "Work_For" ]
+
+def print_annot_file(annot_sent_dic, label):
+    line = 'sent' + str(annot_sent_dic['id']) + '\t' + str(annot_sent_dic['ent1']) + '\t' + good_annot[label] + '\t' \
+           + str(annot_sent_dic['ent2']) + '\t' + '( ' + str(annot_sent_dic['sent']) + ' )'+'\n'
+    return line
+
+
 
 if __name__ == "__main__":
     ds = DataSet('dataset')
@@ -20,8 +29,15 @@ if __name__ == "__main__":
     X, y = ds.train
     clf.fit(X, y)
     X_dev, y_dev = ds.dev
+    annot_data_dev = ds.dev_annot_data
     y_pred = clf.predict(X_dev)
     from sklearn import metrics
     print('f1-WRK-LIV:', metrics.f1_score(y_dev, y_pred, labels=[0, 2], average='weighted'))
     print('f1-NON:', metrics.f1_score(y_dev, y_pred, labels=[1], average='weighted'))
+    with open('annot_pred.txt', 'w') as f:
+        for i, pred in enumerate(y_pred):
+            # print(pred)
+            if pred != 1: #NON
+                line = print_annot_file(annot_data_dev[i], pred)
+                f.write(line)
 
